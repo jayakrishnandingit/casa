733ab634044d22575918affbd735872754e193de
Jayakrishnan Damodaran <jakyjk05@gmail.com>
2021-01-24 19:24:08 +0530

    [HOT FIX]. Add a management command to create an admin user.

---
+++ home/management/commands/createadmin.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.core.auth import get_user_model
+from django.conf import settings
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    help = 'Create an admin user. This is an idempotent command. Only creates iff there is no admin user.'
+
+    def handle(self, *args, **options):
+        admin = User.objects.filter(email=settings.CONTACT_EMAIL)
+        if admin.count() > 0:
+            self.stdout.write(f"Admin with email {settings.CONTACT_EMAIL} exists. Skipping command.")
+            return
+        self.stdout.write(f"Creating admin user. email: {settings.CONTACT_EMAIL}, username: admin.")
+        User.objects.create_superuser(
+            username='admin',
+            email=settings.CONTACT_EMAIL,
+            password='panj@sara'
+        )
+        self.stdout.write("Successfully created admin user.")
